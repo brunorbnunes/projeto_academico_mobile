0c074856e3b293077fa119ce67649bfb0d187e28
brunorbnunes <brunolcbjlf@gmail.com>
2025-06-09 15:13:44 -0300

    Testing Firebase connection

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlc162TaAq8R-K8NQEmpqjL6M-0mdfT9A",
+  authDomain: "projeto-mobile-6f319.firebaseapp.com",
+  projectId: "projeto-mobile-6f319",
+  storageBucket: "projeto-mobile-6f319.appspot.com",
+  messagingSenderId: "210667419478",
+  appId: "1:210667419478:web:f0feff6d56c656d912c8f7",
+  measurementId: "G-D0PVBM0MNY"
+};
+
+export const app = initializeApp(firebaseConfig);
