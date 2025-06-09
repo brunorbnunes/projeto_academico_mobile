a9894e37f1d8cc6e4a14adbc82fd365fb2d1c044
brunorbnunes <brunolcbjlf@gmail.com>
2025-06-09 16:51:31 -0300

    installing prebuild

--- App.js
+++ App.js
@@ -1,28 +1,132 @@
-import './firebaseConfig';
-import { getApps } from 'firebase/app';
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Button, Text, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
-  const [firebaseStatus, setFirebaseStatus] = useState('Testando conexão...');
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
-  useEffect(() => {
-    const apps = getApps();
-    if (apps.length > 0) {
-      setFirebaseStatus('✅ Conectado ao Firebase!');
-    } else {
-      setFirebaseStatus('❌ Falha na conexão com Firebase.');
-    }
-  }, []);
-
+const Login = ({ navigation, route }) => {
+  const [email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20, color: 'green', marginBottom: 20 }}>
-        {firebaseStatus}
-      </Text>
-      <Text style={{ textAlign: 'center', color: '#555' }}>
-        Se aparecer "Conectado ao Firebase!", está tudo certo!
-      </Text>
+    <View>
+      <Text>E-mail: </Text>
+      <TextInput
+        keyboardType="email-address"
+        value={email}
+        onChangeText={text => setEmail(text)}
+        placeholder="Digite seu e-mail"
+      />
+      <Text>Senha: </Text>
+      <TextInput
+        secureTextEntry={true}
+        value={password}
+        onChangeText={text => setPassword(text)}
+        placeholder="Digite sua senha"
+
+      />
+      
+      <Button onPress={() => route.params.funcLogar(true)} title="Logar" />
+      <Button
+        onPress={() => navigation.navigate('Registrar')}
+        title="Registrar"
+      />
     </View>
   );
-}
+};
+
+const Registrar = () => {
+  return <Text>Registrar</Text>;
+};
+
+const Avisos = () => {
+  return <Text>Avisos</Text>;
+};
+
+const Perfil = () => {
+  return <Text>Perfil</Text>;
+};
+
+const Home = ({ navigation, route }) => {
+  return (
+    <View>
+      <Button onPress={() => route.params.funcLogar(false)} title="Deslogar" />
+      <Button onPress={() => navigation.navigate('Perfil')} title="Perfil" />
+    </View>
+  );
+};
+
+/* Logomarca da instituição; textos; títulos(label); banner publicitário rotativo - Kayky */
+const Home_stack = ({ navigation, route }) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        options={{ headerShown: false }}
+        component={Home}
+        initialParams={{ funcLogar: route.params.funcLogar }}
+      />
+      <Stack.Screen name="Perfil" component={Perfil} />
+    </Stack.Navigator>
+  );
+};
+
+const Config = () => {
+  return <Text>Config</Text>;
+};
+
+const Contatos = () => {
+  return <Text>Contatos</Text>;
+};
+
+const Fotos = () => {
+  return <Text>Fotos</Text>;
+};
+
+const App = () => {
+  const [EstaLogado, setLogado] = useState(false);
+  return EstaLogado ? (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="Home"
+          component={() => {
+            return (
+              <Tab.Navigator>
+                <Tab.Screen
+                  name="Home_tab"
+                  options={{ headerShown: false }}
+                  component={Home_stack}
+                  initialParams={{ funcLogar: setLogado }}
+                />
+                <Tab.Screen name="Avisos" component={Avisos} />
+              </Tab.Navigator>
+            );
+          }}
+        />
+        <Drawer.Screen name="Configurações" component={Config} />
+        <Drawer.Screen name="Contatos" component={Contatos} />
+        <Drawer.Screen name="Fotos" component={Fotos} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  ) : (
+
+  /* Elementos visuais (Kayky) - Logomarca da instituição; banner publicitário rotativo; canais de comunicação Elementos Interativos (Isabelle) - input de dados; botão de ação */
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Sistema para curso de alfabetização de jovens e adultos"
+          component={Login}
+          initialParams={{ funcLogar: setLogado }}
+        />
+        <Stack.Screen name="Registrar" component={Registrar} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,21 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBlc162TaAq8R-K8NQEmpqjL6M-0mdfT9A",
   authDomain: "projeto-mobile-6f319.firebaseapp.com",
   projectId: "projeto-mobile-6f319",
-  storageBucket: "projeto-mobile-6f319.appspot.com",
+  storageBucket: "projeto-mobile-6f319.firebasestorage.app",
   messagingSenderId: "210667419478",
   appId: "1:210667419478:web:f0feff6d56c656d912c8f7",
   measurementId: "G-D0PVBM0MNY"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
