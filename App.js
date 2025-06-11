2ac5bd840393ce920132acb6123c0458a175f9ac
brunorbnunes <brunolcbjlf@gmail.com>
2025-06-11 16:22:34 -0300

    import SQlite

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import * as SQLite from 'expo-sqlite';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
